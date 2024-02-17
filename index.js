15c30cd7f7b78356fd8df54b37ab95bc1d9caf3e
Ev11nroo <146498770+Ev11nroo@users.noreply.github.com>
2024-02-17 16:11:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { getCSRFAndAuthenticate, launch, launchProtocol, setUserStatusToUnknown, gameLaunchSuccessful, gameLaunchSuccessful_Protocol } = require('./http')
 const { replicate } = require('./config.json')
-var { gameId } = require('./config.json')
+let { gameId } = require('./config.json')
 const timestamp = Math.floor(Date.now() / 1000);
 
 console.log("Communicating with Roblox...\n")
