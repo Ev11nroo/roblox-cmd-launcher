b5a02f18d64bdb6b2c8efcacb76387b5ec4189c6
roo <146498770+Ev11nroo@users.noreply.github.com>
2024-03-28 19:43:11 -0400

    Update http.js

--- http.js
+++ http.js
@@ -9,18 +9,21 @@ const options = {
     body: 'false'
 };
 
+let playToken;
+
 // send out the HTTP requests
 // get X-CSRF-TOKEN
-function getCSRFAndAuthenticate(unixtime, gameId) {
+function getCSRFAndAuthenticate(unixtime, gameId, privateServerAccessCode) {
     const aquireXCSRF = fetch('https://auth.roblox.com/v2/logout', options)
         .then(async response => { 
           const csrf = await response.headers.get('x-csrf-token');
-          if (csrf) {
-            console.log('Got XCSRF Token successfully!')
-            return csrf
-          } else {
+          if (!csrf) {
             console.error('XCSRF Token could not be grabbed, no cookie provided or servers are having issues.')
+            return 0;
           }
+
+          console.log('Got XCSRF Token successfully!')
+          return csrf
         })
         .catch(err => console.error(err))
 
@@ -38,29 +41,33 @@ function getCSRFAndAuthenticate(unixtime, gameId) {
     
     const getAuthTicket = fetch('https://auth.roblox.com/v1/authentication-ticket', authOptions)
         .then(async response => { 
-          if (await response.status == 200) {
-            console.log('Authenticated successfully with Roblox!')
-          } else {
+          if (await response.status != 200) {
             console.error('Could not authenticate, XCSRF token failed or authentication servers are having issues.')
+            return 0;
           }
-          
+
+          console.log('Authenticated successfully with Roblox!')
+
           const authTicket = response.headers.get('rbx-authentication-ticket')
-          if (authTicket) {
-            console.log('Got Authentication Ticket!')
-
-            const playToken = `roblox-player:1+launchmode:play+gameinfo:${authTicket}+launchtime:${unixtime}+placelauncherurl:https%3A%2F%2Fassetgame.roblox.com%2Fgame%2FPlaceLauncher.ashx%3Frequest%3DRequestGame%26browserTrackerId%3D${browserTrackerId}%26placeId%3D${gameId}%26isPlayTogetherGame%3Dfalse%26joinAttemptId%3D${joinAttemptId}%26joinAttemptOrigin%3DPlayButton+browsertrackerid:${browserTrackerId}+robloxLocale:en_us+gameLocale:en_us+channel:`
-            
-            if (!writeToFile) {
-              console.log("\nHere is your play token:\n", playToken)
-              return 0;
-            }
-
-            fs.writeFile('./playtoken.txt', playToken, err => { if (err) throw err; })
-            console.log('\nWritten your play token to "playtoken.txt"')
-            
-          } else {
+          if (!authTicket) {
             console.error('Could not get ticket, XCSRF token failed or authentication servers are having issues.')
           }
+
+          console.log('Got Authentication Ticket!')
+
+          playToken = `roblox-player:1+launchmode:play+gameinfo:${authTicket}+launchtime:${unixtime}+placelauncherurl:https%3A%2F%2Fassetgame.roblox.com%2Fgame%2FPlaceLauncher.ashx%3Frequest%3DRequestGame%26browserTrackerId%3D${browserTrackerId}%26placeId%3D${gameId}%26isPlayTogetherGame%3Dtrue%26joinAttemptId%3D${joinAttemptId}%26joinAttemptOrigin%3DPlayButton+browsertrackerid:${browserTrackerId}+robloxLocale:en_us+gameLocale:en_us+channel:`
+
+          if (privateServerAccessCode != null) {
+            playToken = `roblox-player:1+launchmode:play+gameinfo:${authTicket}+launchtime:${unixtime}+placelauncherurl:https%3A%2F%2Fassetgame.roblox.com%2Fgame%2FPlaceLauncher.ashx%3Frequest%3DRequestPrivateGame%26browserTrackerId%3D${browserTrackerId}%26placeId%3D${gameId}%26accessCode%3D${privateServerAccessCode}%26joinAttemptId%3D${joinAttemptId}%26joinAttemptOrigin%3DprivateServerListJoin+browsertrackerid:${browserTrackerId}+robloxLocale:en_us+gameLocale:en_us+channel:`
+          }
+
+          if (!writeToFile) {
+            console.log("\nHere is your play token:\n", playToken)
+            return 0;
+          }
+
+          fs.writeFile('./playtoken.txt', playToken, err => { if (err) throw err; })
+          console.log('\nWritten your play token to "playtoken.txt"')
         })
         .catch(err => console.error(err));
     })
@@ -70,11 +77,12 @@ function getCSRFAndAuthenticate(unixtime, gameId) {
 function launch() {
    fetch('https://assetgame.roblox.com/game/report-event?name=GameLaunchAttempt_Unknown', options)
         .then(async response => { 
-          if (await response.status == 200) {
-            console.log('Successfully told that a game wants to be launched')
-          } else {
+          if (await response.status != 200) {
             console.error('Could not complete, no cookie provided or servers are having issues.')
+            return 0;
           }
+
+          console.log('Successfully told that a game wants to be launched')
         })
         .catch(err => console.error(err)); 
 } 
@@ -83,11 +91,12 @@ function launch() {
 function launchProtocol() {
     fetch('https://assetgame.roblox.com/game/report-event?name=GameLaunchAttempt_Unknown_Protocol', options)
         .then(async response => { 
-        if (await response.status == 200) {
-          console.log('Successfully told to give launch parameters')
-        } else {
+        if (await response.status != 200) {
           console.error('Could not complete, no cookie provided or servers are having issues.')
+          return 0;
         }
+
+        console.log('Successfully told to give launch parameters')
         })
         .catch(err => console.error(err));  
 } 
@@ -96,11 +105,12 @@ function launchProtocol() {
 function setUserStatusToUnknown() {
     fetch('https://www.roblox.com/client-status/set?status=Unknown', options)
         .then(async response => { 
-        if (await response.status == 200) {
-          console.log('Set user status to "Unknown"')
-        } else {
+        if (await response.status != 200) {
           console.error('Could not complete, no cookie provided or servers are having issues.')
+          return 0;
         }
+
+        console.log('Set user status to "Unknown"')
         })
         .catch(err => console.error(err));
 } 
@@ -109,11 +119,12 @@ function setUserStatusToUnknown() {
 function gameLaunchSuccessful() {
   fetch('https://assetgame.roblox.com/game/report-event?name=GameLaunchSuccessWeb_Unknown', options)
     .then(response => { 
-    if (response.status == 200) {
-      console.log('Told Roblox that game launch was successful')
-    } else {
+    if (response.status != 200) {
       console.error('Could not complete, no cookie provided or servers are having issues.')
+      return 0;
     }
+
+    console.log('Told Roblox that game launch was successful')
     })
     .catch(err => console.error(err));
 }
@@ -122,21 +133,15 @@ function gameLaunchSuccessful() {
 function gameLaunchSuccessful_Protocol() {
   fetch('https://assetgame.roblox.com/game/report-event?name=GameLaunchSuccessWeb_Unknown_Protocol', options)
     .then(response => { 
-    if (response.status == 200) {
-      console.log('Told Roblox that game launch was successful (with protocol!!!!!!!!)')
-    } else {
+    if (response.status != 200) {
       console.error('Could not complete, no cookie provided or servers are having issues.')
+      return 0;
     }
+
+    console.log('Told Roblox that game launch was successful (with protocol!!!!!!!!)')
     })
     .catch(err => console.error(err));
 }
-
-
-// check current user status (doesnt even work here lo)
-/*fetch('https://www.roblox.com/client-status?_=1706387466', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));*/
 
 module.exports = {
     getCSRFAndAuthenticate,
