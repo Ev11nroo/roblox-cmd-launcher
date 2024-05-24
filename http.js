5fad0c7649cbd7ae40b7d25b0680dcd47099b5fb
roo <146498770+Ev11nroo@users.noreply.github.com>
2024-05-24 15:06:09 -0400

    Update http.js

--- http.js
+++ http.js
@@ -1,6 +1,10 @@
 const { cookie, browserTrackerId, joinAttemptId, writeToFile} = require('./config.json')
 const fs = require('fs')
 
+if (fs.existsSync('./playtoken.txt')) {
+  fs.unlinkSync('./playtoken.txt', err => { if (err) throw err; })
+}
+
 const options = {
     method: 'POST',
     headers: {
