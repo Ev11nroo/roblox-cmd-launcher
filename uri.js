b606f6ea0688c67f4a00d2bd4d119b205b88ac03
roo <146498770+Ev11nroo@users.noreply.github.com>
2025-02-14 23:07:07 -0500

    use characters in uri instead of encoded set

--- uri.js
+++ uri.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 function createURI(authTicket, privateServerAccessCode, friendId, unixtime, gameId, browserTrackerId, joinAttemptId) {
     let initalUri = `roblox-player:1+launchmode:play+launchtime:${unixtime}+`;
-    let placeLauncherUrl = `https%3A%2F%2Fassetgame.roblox.com%2Fgame%2FPlaceLauncher.ashx%3Frequest%3DRequestGame%26placeId%3D${gameId}%26`;
+    let placeLauncherUrl = `https://assetgame.roblox.com/game/PlaceLauncher.ashx?request=RequestGame&placeId=${gameId}&`;
     
     if (cookie != null || authTicket != null) {
         initalUri += `gameinfo:${authTicket}+`;
@@ -11,22 +11,22 @@ function createURI(authTicket, privateServerAccessCode, friendId, unixtime, game
     
     if (browserTrackerId != null) {
         initalUri += `browsertrackerid:${browserTrackerId}+`
-        placeLauncherUrl += `browserTrackerId%3D${browserTrackerId}%26`
+        placeLauncherUrl += `browserTrackerId=${browserTrackerId}&`
     }
     
     if (joinAttemptId != null) {
-        placeLauncherUrl += `joinAttemptId%3D${joinAttemptId}%26`
+        placeLauncherUrl += `joinAttemptId=${joinAttemptId}&`
     }
     
     if (privateServerAccessCode != null) {
-        placeLauncherUrl = placeLauncherUrl.replace("request%3DRequestGame", "request%3DRequestPrivateGame");
-        placeLauncherUrl += `accessCode%3D${privateServerAccessCode}%26`;
+        placeLauncherUrl = placeLauncherUrl.replace("request=RequestGame", "request=RequestPrivateGame");
+        placeLauncherUrl += `accessCode=${privateServerAccessCode}&`;
     }
     
     if (friendId != null) {
-        placeLauncherUrl = placeLauncherUrl.replace("request%3DRequestGame", "request%3DRequestFollowUser")
-        placeLauncherUrl = placeLauncherUrl.replace(`placeId%3D${gameId}`, "")
-        placeLauncherUrl += `userId%3D${friendId}%26`
+        placeLauncherUrl = placeLauncherUrl.replace("request=RequestGame", "request=RequestFollowUser")
+        placeLauncherUrl = placeLauncherUrl.replace(`placeId=${gameId}`, "")
+        placeLauncherUrl += `userId=${friendId}&`
     }
 
     initalUri += "placelauncherurl:"
