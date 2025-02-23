97cc83e8267dc1dbbff8695d576ab7e857aedb79
roo <146498770+Ev11nroo@users.noreply.github.com>
2025-02-23 14:01:46 -0500

    make semicolons consistent

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { cookie, writeToFile } = require('./config.json')
-let { gameId, privateServerAccessCode, friendId } = require('./config.json')
-const fs = require('fs')
+const { cookie, writeToFile } = require('./config.json');
+let { gameId, privateServerAccessCode, friendId } = require('./config.json');
+const fs = require('fs');
 
 const unixtime = Math.floor(Date.now() / 1000);
 
@@ -8,22 +8,22 @@ const currentVersion = fs.readFileSync('./version.txt', 'utf8', (err) => { if (e
 let githubVersion;
 
 if (fs.existsSync('./uri.txt')) {
-  fs.unlinkSync('./uri.txt', err => { if (err) throw err; })
+  fs.unlinkSync('./uri.txt', err => { if (err) throw err; });
 }
 
 for (i = process.argv.length; i >= 1; i--) {
     switch (process.argv[i]) {
         case '-g':
         case '--gameId':
-            gameId = process.argv[i + 1]
+            gameId = process.argv[i + 1];
             break;
         case '-p':
         case '--accessCode':
-            privateServerAccessCode = process.argv[i + 1]
+            privateServerAccessCode = process.argv[i + 1];
             break;
         case '-f':
         case '--friendId':
-            friendId = process.argv[i + 1]
+            friendId = process.argv[i + 1];
             break;
         case '-h':
         case '--help':
@@ -35,7 +35,7 @@ for (i = process.argv.length; i >= 1; i--) {
                         '                        (NOTE: Private server MUST exist within the Game ID. Access to the private server is required.)\n' +
                         '    -h, --help          Show this help menu\n' + 
                         '    -f, --friendId      The user ID to follow to a game\n'
-                    )
+                    );
             return 0;
     }
 }
@@ -47,7 +47,7 @@ fetch('https://raw.githubusercontent.com/Ev11nroo/roblox-cmd-launcher/refs/heads
     if (currentVersion < githubVersion) {
         console.log('This version is outdated, please update from https://github.com/Ev11nroo/roblox-cmd-launcher')
     }
-})
+});
 
 function createURI(authTicket, privateServerAccessCode, friendId, unixtime, gameId) {
     let initalUri = `roblox-player:1+launchmode:play+launchtime:${unixtime}+`;
@@ -63,30 +63,30 @@ function createURI(authTicket, privateServerAccessCode, friendId, unixtime, game
     }
     
     if (friendId != null) {
-        placeLauncherUrl = placeLauncherUrl.replace("request=RequestGame", "request=RequestFollowUser")
-        placeLauncherUrl = placeLauncherUrl.replace(`placeId=${gameId}`, "")
-        placeLauncherUrl += `userId=${friendId}&`
+        placeLauncherUrl = placeLauncherUrl.replace("request=RequestGame", "request=RequestFollowUser");
+        placeLauncherUrl = placeLauncherUrl.replace(`placeId=${gameId}`, "");
+        placeLauncherUrl += `userId=${friendId}&`;
     }
 
-    initalUri += "placelauncherurl:"
-    let uri = `${initalUri}${placeLauncherUrl}`
+    initalUri += "placelauncherurl:";
+    let uri = `${initalUri}${placeLauncherUrl}`;
     
     if (!writeToFile) {
-        console.log("\nURI:", uri)
+        console.log("\nURI:", uri);
         return 0;
     }
 
-    fs.writeFile('./uri.txt', uri, err => { if (err) throw err; })
-    console.log('\nWritten your URI to "uri.txt"')
+    fs.writeFile('./uri.txt', uri, err => { if (err) throw err; });
+    console.log('\nWritten your URI to "uri.txt"');
 }
 
 if (cookie == null) {
-  createURI(null, privateServerAccessCode, friendId, timestamp, gameId)
+  createURI(null, privateServerAccessCode, friendId, timestamp, gameId);
   return 0;
 }
 
 if (friendId != null && privateServerAccessCode != null) {
-  console.error("privateServerAccessCode requires to be 'null' to use friendId (5)")
+  console.error("privateServerAccessCode requires to be 'null' to use friendId (5)");
   return 5;
 }
 
@@ -103,12 +103,12 @@ const aquireXCSRF = fetch('https://auth.roblox.com/v2/logout', options)
     .then(async response => { 
         const csrf = await response.headers.get('x-csrf-token');
         if (!csrf) {
-            console.error(`XCSRF Token could not be grabbed. (1): ${response.status}`)
+            console.error(`XCSRF Token could not be grabbed. (1): ${response.status}`);
             return 1;
         }
-        return csrf
+        return csrf;
         })
-    .catch(err => console.error(err))
+    .catch(err => console.error(err));
 
 // get authentication token to launch game
 aquireXCSRF.then(csrf => {
@@ -125,17 +125,17 @@ aquireXCSRF.then(csrf => {
     const getAuthTicket = fetch('https://auth.roblox.com/v1/authentication-ticket', authOptions)
         .then(async response => { 
           if (await response.status != 200) {
-            console.error(`Could not authenticate with Roblox. (2): ${response.status}`)
+            console.error(`Could not authenticate with Roblox. (2): ${response.status}`);
             return 2;
           }
 
-          const authTicket = response.headers.get('rbx-authentication-ticket')
+          const authTicket = response.headers.get('rbx-authentication-ticket');
           if (!authTicket) {
-            console.error(`Could not get ticket. (3): ${response.status}`)
+            console.error(`Could not get ticket. (3): ${response.status}`);
               return 3;
           }
 
-          createURI(authTicket, privateServerAccessCode, friendId, unixtime, gameId)
+          createURI(authTicket, privateServerAccessCode, friendId, unixtime, gameId);
         })
         .catch(err => console.error(err));
 })
