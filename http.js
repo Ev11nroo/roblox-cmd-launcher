5b631b35afc14b35815313655d997d5be6dda244
roo <146498770+Ev11nroo@users.noreply.github.com>
2025-04-27 11:58:58 -0400

    make tab sizes consistent

--- http.js
+++ http.js
@@ -3,14 +3,14 @@ const { createURI } = require('./uri');
 const fs = require('fs');
 
 if (fs.existsSync('./uri.txt')) {
-  fs.unlinkSync('./uri.txt', err => { if (err) throw err; });
+    fs.unlinkSync('./uri.txt', err => { if (err) throw err; });
 }
 
 const options = {
     method: 'POST',
     headers: {
-      cookie: `${cookie}`,
-      referer: 'https://www.roblox.com/',
+        cookie: `${cookie}`,
+        referer: 'https://www.roblox.com/',
     },
     body: 'false'
 };
@@ -22,14 +22,14 @@ let playToken;
 function getCSRFAndAuthenticate(unixtime, gameId, privateServerAccessCode, friendId) {
     const aquireXCSRF = fetch('https://auth.roblox.com/v2/logout', options)
         .then(async response => { 
-          const csrf = await response.headers.get('x-csrf-token');
-          if (!csrf) {
-            console.error(`XCSRF Token could not be grabbed. (1): ${response.status}`);
-            return 1;
-          }
+            const csrf = await response.headers.get('x-csrf-token');
+            if (!csrf) {
+                console.error(`XCSRF Token could not be grabbed. (1): ${response.status}`);
+                return 1;
+            }
 
-          console.log('Got XCSRF Token successfully!');
-          return csrf;
+            console.log('Got XCSRF Token successfully!');
+            return csrf;
         })
         .catch(err => console.error(err));
 
@@ -38,30 +38,30 @@ function getCSRFAndAuthenticate(unixtime, gameId, privateServerAccessCode, frien
         const authOptions = {
         method: 'POST',
         headers: {
-          cookie: `${cookie}`,
-          referer: 'https://www.roblox.com/',
-          'x-csrf-token': `${csrf}`
+            cookie: `${cookie}`,
+            referer: 'https://www.roblox.com/',
+            'x-csrf-token': `${csrf}`
         },
         body: 'false'
         };
     
     const getAuthTicket = fetch('https://auth.roblox.com/v1/authentication-ticket', authOptions)
         .then(async response => { 
-          if (await response.status != 200) {
-            console.error(`Could not authenticate with Roblox. (2): ${response.status}`);
-            return 2;
-          }
+            if (await response.status != 200) {
+                console.error(`Could not authenticate with Roblox. (2): ${response.status}`);
+                return 2;
+            }
 
-          console.log('Authenticated successfully with Roblox!');
+            console.log('Authenticated successfully with Roblox!');
 
-          const authTicket = response.headers.get('rbx-authentication-ticket');
-          if (!authTicket) {
-            console.error(`Could not get ticket. (3): ${response.status}`);
-            return 3;
-          }
+            const authTicket = response.headers.get('rbx-authentication-ticket');
+            if (!authTicket) {
+                console.error(`Could not get ticket. (3): ${response.status}`);
+                return 3;
+            }
 
-          console.log('Got Authentication Ticket!');
-          createURI(authTicket, privateServerAccessCode, friendId, unixtime, gameId, browserTrackerId, joinAttemptId);
+            console.log('Got Authentication Ticket!');
+            createURI(authTicket, privateServerAccessCode, friendId, unixtime, gameId, browserTrackerId, joinAttemptId);
         })
         .catch(err => console.error(err));
     })
@@ -71,12 +71,12 @@ function getCSRFAndAuthenticate(unixtime, gameId, privateServerAccessCode, frien
 function launch() {
    fetch('https://assetgame.roblox.com/game/report-event?name=GameLaunchAttempt_Unknown', options)
         .then(async response => { 
-          if (await response.status != 200) {
-            console.error(`Could not complete request. (4): ${response.status}`);
-            return 4;
-          }
+            if (await response.status != 200) {
+                console.error(`Could not complete request. (4): ${response.status}`);
+                return 4;
+            }
 
-          console.log('Successfully told that a game wants to be launched');
+            console.log('Successfully told that a game wants to be launched');
         })
         .catch(err => console.error(err)); 
 } 
@@ -85,12 +85,12 @@ function launch() {
 function launchProtocol() {
     fetch('https://assetgame.roblox.com/game/report-event?name=GameLaunchAttempt_Unknown_Protocol', options)
         .then(async response => { 
-        if (await response.status != 200) {
-          console.error(`Could not complete request. (4) ${response.status}`);
-          return 4;
-        }
+            if (await response.status != 200) {
+                console.error(`Could not complete request. (4) ${response.status}`);
+                return 4;
+            }
 
-        console.log('Successfully told to give launch parameters');
+            console.log('Successfully told to give launch parameters');
         })
         .catch(err => console.error(err));  
 } 
@@ -99,12 +99,12 @@ function launchProtocol() {
 function setUserStatusToUnknown() {
     fetch('https://www.roblox.com/client-status/set?status=Unknown', options)
         .then(async response => { 
-        if (await response.status != 200) {
-          console.error(`Could not complete request. (4) ${response.status}`);
-          return 4;
-        }
+            if (await response.status != 200) {
+                console.error(`Could not complete request. (4) ${response.status}`);
+                return 4;
+            }
 
-        console.log('Set user status to "Unknown"');
+            console.log('Set user status to "Unknown"');
         })
         .catch(err => console.error(err));
 } 
@@ -113,12 +113,12 @@ function setUserStatusToUnknown() {
 function gameLaunchSuccessful() {
   fetch('https://assetgame.roblox.com/game/report-event?name=GameLaunchSuccessWeb_Unknown', options)
     .then(response => { 
-    if (response.status != 200) {
-      console.error(`Could not complete request. (4) ${response.status}`);
-      return 4;
-    }
+        if (response.status != 200) {
+            console.error(`Could not complete request. (4) ${response.status}`);
+            return 4;
+        }
 
-    console.log('Told Roblox that game launch was successful');
+        console.log('Told Roblox that game launch was successful');
     })
     .catch(err => console.error(err));
 }
@@ -127,12 +127,12 @@ function gameLaunchSuccessful() {
 function gameLaunchSuccessful_Protocol() {
   fetch('https://assetgame.roblox.com/game/report-event?name=GameLaunchSuccessWeb_Unknown_Protocol', options)
     .then(response => { 
-    if (response.status != 200) {
-      console.error(`Could not complete request. (4) ${response.status}`);
-      return 4;
-    }
+        if (response.status != 200) {
+            console.error(`Could not complete request. (4) ${response.status}`);
+            return 4;
+        }
 
-    console.log('Told Roblox that game launch was successful (with protocol!!!!!!!!)');
+        console.log('Told Roblox that game launch was successful (with protocol!!!!!!!!)');
     })
     .catch(err => console.error(err));
 }
