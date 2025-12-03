61f79fd1f3044d2f8f99756bc43bb4f672cddab2
roo <146498770+Ev11nroo@users.noreply.github.com>
2025-12-02 23:19:20 -0500

    add authticket in uri

--- uri.py
+++ uri.py
@@ -1,2 +1,19 @@
-def createUri(authTicket, privateServerAccessCode, friendId, unixtime, gameId, serverId):
+import json
+import time
+
+config = json.load(open("config.json"))
+cookie = config["cookie"]
+writeToFile = config["writeToFile"]
+command = config["command"]
+
+def createUri(authTicket, privateServerAccessCode, friendId, gameId, serverId):
+    unixtime = round(time.time())
+
+    initalUri = f"roblox-player:1+launchmode:play+launchtime:{unixtime}+"
+    placeLauncherUrl = f"https://assetgame.roblox.com/game/PlaceLauncher.ashx?request=RequestGame&placeId={gameId}&"
+
+    if (cookie != None) or (authTicket != None):
+        initalUri += f"gameinfo:{authTicket}+"
+
+    print(initalUri)
     return
