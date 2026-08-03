3fdc9177d76698f27f73839a3242bd2ade673d35
roo <146498770+Ev11nroo@users.noreply.github.com>
2026-08-03 17:29:23 -0400

    prepare login requests

--- login.js
+++ login.js
@@ -1,3 +1,52 @@
 let config = require("./config.json");
 const fs = require("fs");
 
+let statusOptions = {
+    method: "POST",
+    headers: {
+        "x-csrf-token": `${csrf}`
+    },
+    body: JSON.parse({
+        "code": `${code}`,
+        "privateKey": `${privateKey}`,
+    })
+}
+
+async function checkForCsrf(response) {
+    
+}
+
+async function createToken() {
+    let request;
+
+    try {
+        request = await fetch("https://apis.roblox.com/auth-token-service/v1/login/create", {
+            method: "POST"
+        });
+    } catch (e) {
+        console.error("Failed to complete request (7)");
+        return 7;
+    }
+}
+
+async function checkToken(code, privateKey, csrf) {
+    let request;
+
+    try {
+        request = await fetch("https://apis.roblox.com/auth-token-service/v1/login/status", statusOptions);
+    } catch (e) {
+        console.error("Failed to complete request (7)");
+        return 7;
+    }
+}
+
+async function invalidateToken(code) {
+    let request;
+
+    try {
+        request = await fetch("https://apis.roblox.com/auth-token-service/v1/login/cancel", statusOptions);
+    } catch (e) {
+        console.error("Failed to complete request (7)");
+        return 7;
+    }
+}
